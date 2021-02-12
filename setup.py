a8198b3cc716594f7e83ba5935b47472da71b21b
Francisco Penedo <fran@franpenedo.com>
2021-02-12 21:56:28 +0100

    Added requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+config = {
+    "description": "Assume-Guarantee Contract Mining Using MILP",
+    "url": "",
+    "author": "Fran Penedo",
+    "author_email": "fran@franpenedo.com",
+    "version": "1.0.0",
+    "install_requires": [
+        "numpy>=1.20.1",
+        "gurobipy>=9.1.1",
+        "scipy>=1.6.0",
+        # "templogic@git+git://github.com/fran-penedo/templogic.git@v2.0.3",
+        "templogic>=2.0.3",
+        "matplotlib>=3.3.4",
+    ],
+    "packages": find_packages(),
+    "scripts": [],
+    "entry_points": {},
+    "name": "agmilp",
+}
+
+setup(**config)
